9ab32ad48cac0430d826f4b0f31f8157b8c6c848
Nelson Ponzoni <npcuadra@gmail.com>
2016-08-08 16:41:13 -0300

    setup

--- setup.py
+++ setup.py
@@ -1,22 +1,19 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from setuptools import setup, find_packages
 
 setup(
     name="cupydle",
-
-    version="0.1.0",
-
+    version="1.0.0",
     author="Ponzoni Nelson",
     author_email="npcuadra@gmail.com",
     url="https://github.com/lerker/cupydle",
-
     packages=find_packages(),
     include_package_data=True,
     scripts=[],
-
     license="LICENSE",
-
-    description="Framework of deep learning for CUDA",
+    description="Framework of deep learning GP-GPU",
     #long_description=open("README.md").read(),
-
     install_requires=open("requirements.txt").read().split()
 )
