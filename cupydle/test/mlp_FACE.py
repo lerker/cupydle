d96b70ac828eba19fe4eee02fb0e0065b7d32c68
Nelson Ponzoni <npcuadra@gmail.com>
2016-09-13 22:29:56 -0300

    parametrizacion de la ejecucion de MLP para FACE

--- cupydle/test/mlp_FACE.py
+++ cupydle/test/mlp_FACE.py
@@ -37,10 +37,27 @@ from cupydle.dnn.mlp import MLP
 
 if __name__ == "__main__":
 
-    directorioActual= os.getcwd()                                   # directorio actual de ejecucion
+    parser = argparse.ArgumentParser(description='Prueba de un MLP sobre FACE.')
+    parser.add_argument('-d', '--directorio',   type=str,   dest="directorio",  default='test_MLP', help="Carpeta donde se almacena la corrida actual")
+    parser.add_argument('--dataset',            type=str,   dest="dataset",     default='videos_clases_procesados_zscore_minmax.npz', help="Archivo donde esta el dataset, [videos, clases].npz")
+    parser.add_argument('-l', '--capas',        type=int,   dest="capas",       required=True,      nargs='+',          help="Capas del MLP, #entrada, #oculta1, .... , #salida")
+    parser.add_argument('-b', '--batchsize',    type=int,   dest="tambatch",    default=10,         required=False,     help="Tamanio del minibatch para el entrenamiento")
+    parser.add_argument('-e', '--epocas',       type=int,   dest="epocas",      default=10,         required=False,     help="Cantidad de epocas")
+    argumentos = parser.parse_args()
+
+    # parametros pasados por consola
+    directorio_ejecucion  = argumentos.directorio
+    capas                 = argumentos.capas
+    dataset               = argumentos.dataset
+    tambatch              = argumentos.tambatch
+    epocas                = argumentos.epocas
+
+
+    # configuraciones con respecto a los directorios
+    directorioActual= os.getcwd()                                  # directorio actual de ejecucion
     rutaTest        = directorioActual + '/cupydle/test/face/'     # sobre el de ejecucion la ruta a los tests
     rutaDatos       = directorioActual + '/cupydle/data/DB_face/'  # donde se almacenan la base de datos
-    carpetaTest     = 'test_MLP/'                                   # carpeta a crear para los tests
+    carpetaTest     = directorio_ejecucion + '/'                   # carpeta a crear para los tests
     rutaCompleta    = rutaTest + carpetaTest
 
     if not os.path.exists(rutaCompleta):        # si no existe la crea
@@ -51,64 +68,53 @@ if __name__ == "__main__":
         print("Creando la base de datos en:", rutaDatos)
         os.makedirs(rutaDatos)
 
-    setName = "face"
-
-    parser = argparse.ArgumentParser(description='Prueba de un MLP sobre FACE.')
-    parser.add_argument('-g', '--guardar', action="store_true", dest="guardar", help="desea guardar (correr desde cero)", default=False)
-    parser.add_argument('-m', '--modelo', action="store", dest="modelo", help="nombre del binario donde se guarda/abre el modelo", default="capa1.pgz")
-    args = parser.parse_args()
-
-    guardar = args.guardar
-    modelName = args.modelo
-    #modelName = 'capa1.pgz'
-
-    b = numpy.load(rutaDatos + 'videos_clases_procesados_zscore_minmax.npz')
+    # se cargan  los datos, debe ser un archivo comprimido, en el cual los
+    # arreglos estan en dos archivos, 'videos' y 'clases'
+    b = numpy.load(rutaDatos + dataset)
     videos = b['videos']
     clases = b['clases']
-    del b
-
-    # obtengo todos los subconjuntos
-    #train_img,  train_labels= mn.get_training()
-    #test_img,   test_labels = mn.get_testing()
-    #val_img,    val_labels  = mn.get_validation()
+    del b #libera memoria
 
     # lista de tupas, [(x_trn, y_trn), ...]
     # los datos estan normalizados...
-    #datos = [(videos[0:20,0:], clases[0:20]), (videos[21:,0:],clases[21:])]
-    datos = [(videos, clases), (videos[100:200,0:],clases[100:200]), (videos[200:300,0:],clases[200:300:])]
+    datos = []
+    datosTRN = (videos, clases)
+    datosVAL = (videos[100:200,0:],clases[100:200])
+    datosTST = (videos[200:300,0:],clases[200:300:])
 
-    batchSize = 10
-    n_epochs = 1
-
-    # [unidades_de_entrada, primer_capa_oculta, segunda_capa_oculta, salida]
-    unidadesCapas = [230300, 500, 100, 10]
+    datos.append(datosTRN); datos.append(datosVAL); datos.append(datosTST)
 
     # creo la red
-    #MNIST.plot_one_digit(train_img.get_value()[0])
-    clasificador = MLP( clasificacion=True,
-                        rng=None,
-                        ruta=rutaCompleta)
+    clasificador = MLP(clasificacion=True,
+                       rng=None,
+                       ruta=rutaCompleta)
 
+    # se agregan los parametros
     clasificador.setParametroEntrenamiento({'tasaAprendizaje':0.01})
     clasificador.setParametroEntrenamiento({'regularizadorL1':0.00})
     clasificador.setParametroEntrenamiento({'regularizadorL2':0.0001})
     clasificador.setParametroEntrenamiento({'momento':0.0})
     clasificador.setParametroEntrenamiento({'activationfuntion':sigmoideaTheano()})
-    clasificador.setParametroEntrenamiento({'epocas':10})
+    clasificador.setParametroEntrenamiento({'epocas':epocas})
     clasificador.setParametroEntrenamiento({'toleranciaError':0.08})
 
-    clasificador.agregarCapa(unidadesEntrada=784, unidadesSalida=500, clasificacion=False, activacion=sigmoideaTheano(), pesos=None, biases=None)
-    clasificador.agregarCapa(unidadesSalida=10, clasificacion=True, pesos=None, biases=None)
+    # agrego tantas capas desee de regresion
+    # la primera es la que indica la entrada
+    # las intermedias son de regresion
+    # la ultima es de clasificaicon
+clasificador.agregarCapa(unidadesEntrada=capas[0], unidadesSalida=capas[1], clasificacion=False, activacion=sigmoideaTheano(), pesos=None, biases=None)
+    for idx, capa in enumerate(capas[1:-1]):
+        clasificador.agregarCapa(unidadesSalida=capa, clasificacion=False, activacion=sigmoideaTheano(), pesos=None, biases=None)
+    clasificador.agregarCapa(unidadesSalida=capas[-1], clasificacion=True, pesos=None, biases=None)
 
     T = temporizador()
     inicio = T.tic()
 
-
-
+    # se entrena la red
     clasificador.train( trainSet=datos[0],
                         validSet=datos[1],
                         testSet=datos[2],
-                        batch_size=batchSize)
+                        batch_size=tambatch)
 
     final = T.toc()
     print("Tiempo total para entrenamiento: {}".format(T.transcurrido(inicio, final)))
