deb317e8f81f2f1919d019b7cf897cda20787daf
Nelson Ponzoni <npcuadra@gmail.com>
2016-09-19 17:40:16 -0300

    parametrizacion del MPL en RML

--- corredor.sh
+++ corredor.sh
@@ -9,11 +9,18 @@ var="${var}---------------------------------------------------------------------
 ARCHIVO_LOG="log$(date '+_%d%m_%H%M')"
 echo -e $var > ${ARCHIVO_LOG}
 
-data=$1
 ### ejecutar
-#python3 cupydle/test/dbn_FACE_comparativo.py --directorio test_DBN_PCA_85 --dataset $1 -b 10 --epocasDBN 15 --epocasMLP 50 --capas 85 100 50 25 6 -mrd 2>&1 | tee -a ${ARCHIVO_LOG}
+# mlp comun
+#python3 optirun python3 cupydle/test/mlp_FACE.py --directorio "test_MLP" --dataset "all_videos_features_clases_shuffled_PCA85_minmax.npz" -l 85 50 6 --lepocaTRN 50 -lrTRN 0.01 2>&1 | tee -a ${ARCHIVO_LOG}
+
+# dbn comun
 #python3 cupydle/test/dbn_FACE.py --directorio "test_${data}" --dataset ${data} -b 10 --epocasDBN 15 --epocasMLP 50 --capas 85 100 50 25 6 2>&1 | tee -a ${ARCHIVO_LOG}
-python3 cupydle/test/dbn_FACE_validacionCruzada.py --directorio "test_DBN" --dataset "all_videos_features_clases_shuffled_PCA85_minmax.npz" -l 85 50 6 --lepocaTRN 5 --lepocaFIT 10 -lrTRN 0.01 --folds 6
+
+# dbn que compara un mpl con una dbn entrenadas por separado, puede hacerse ahora haciendo primero un fit y luego un train
+#python3 cupydle/test/dbn_FACE_comparativo.py --directorio test_DBN_PCA_85 --dataset $1 -b 10 --epocasDBN 15 --epocasMLP 50 --capas 85 100 50 25 6 -mrd 2>&1 | tee -a ${ARCHIVO_LOG}
+
+# dbn con validacion cruzada
+python3 cupydle/test/dbn_FACE_validacionCruzada.py --directorio "test_DBN" --dataset "all_videos_features_clases_shuffled_PCA85_minmax.npz" -l 85 50 6 --lepocaTRN 5 --lepocaFIT 10 -lrTRN 0.01 --folds 6 2>&1 | tee -a ${ARCHIVO_LOG}
 
 FECHA_ARCHIVO_FIN=$(date)
 var2=""
--- cupydle/test/mlp_FACE.py
+++ cupydle/test/mlp_FACE.py
@@ -16,7 +16,7 @@ __status__      = "Production"
 
 """
 
-import numpy
+import numpy as np
 import time
 import os
 import sys
@@ -37,45 +37,60 @@ from cupydle.dnn.mlp import MLP
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Prueba de un MLP sobre FACE.')
-    parser.add_argument('-d', '--directorio',   type=str,   dest="directorio",  default='test_MLP', help="Carpeta donde se almacena la corrida actual")
-    parser.add_argument('--dataset',            type=str,   dest="dataset",     default='videos_clases_procesados_zscore_minmax.npz', help="Archivo donde esta el dataset, [videos, clases].npz")
-    parser.add_argument('-l', '--capas',        type=int,   dest="capas",       required=True,      nargs='+',          help="Capas del MLP, #entrada, #oculta1, .... , #salida")
-    parser.add_argument('-b', '--batchsize',    type=int,   dest="tambatch",    default=10,         required=False,     help="Tamanio del minibatch para el entrenamiento")
-    parser.add_argument('-e', '--epocas',       type=int,   dest="epocas",      default=10,         required=False,     help="Cantidad de epocas")
+    parser = argparse.ArgumentParser(description='Prueba de un MLP sobre KLM')
+    parser.add_argument('--directorio',       type=str,   dest="directorio",     default='test_MLP', required=None,  help="Carpeta donde se almacena la corrida actual")
+    parser.add_argument('--dataset',          type=str,   dest="dataset",        default=None,       required=True,  help="Archivo donde esta el dataset, [videos, clases].npz")
+    parser.add_argument('-l', '--capas',      type=int,   dest="capas",          default=None,       required=True,  nargs='+', help="Capas de unidades [visibles, ocultas1.. ocultasn]")
+    parser.add_argument('-let', '--lepocaTRN',type=int,   dest="epocasTRN",      default=10,         required=False, help="cantidad de epocas de entrenamiento")
+    parser.add_argument('-b', '--batchsize',  type=int,   dest="tambatch",       default=10,         required=False, help="Tamanio del minibatch para el entrenamiento")
+    parser.add_argument('-p', '--porcentaje', type=float, dest="porcentaje",     default=0.8,        required=False, help="Porcentaje en que el conjunto de entrenamiento se detina para entrenar y testeo")
+    parser.add_argument('-lrTRN',             type=float, dest="tasaAprenTRN",   default=0.01,       required=False, help="Tasa de aprendizaje para la red")
+    parser.add_argument('--nombre',           type=str,   dest="nombre",         default=None,       required=False, help="Nombre del modelo")
+    parser.add_argument('--reguL1',           type=float, dest="regularizadorL1",default=0.0,        required=False, help="Parametro regularizador L1 para el costo del ajuste")
+    parser.add_argument('--reguL2',           type=float, dest="regularizadorL2",default=0.0,        required=False, help="Parametro regularizador L2 para el costo del ajuste")
+    parser.add_argument('--momentoTRN',       type=float, dest="momentoTRN",     default=0.0,        required=False, help="Tasa de momento para la etapa de entrenamiento")
+
     argumentos = parser.parse_args()
 
     # parametros pasados por consola
-    directorio_ejecucion  = argumentos.directorio
-    capas                 = argumentos.capas
-    dataset               = argumentos.dataset
-    tambatch              = argumentos.tambatch
-    epocas                = argumentos.epocas
+    directorio      = argumentos.directorio
+    dataset         = argumentos.dataset
+    capas           = argumentos.capas
+    epocasTRN       = argumentos.epocasTRN
+    tambatch        = argumentos.tambatch
+    porcentaje      = argumentos.porcentaje
+    tasaAprenTRN    = argumentos.tasaAprenTRN
+    nombre          = argumentos.nombre
+    regularizadorL1 = argumentos.regularizadorL1
+    regularizadorL2 = argumentos.regularizadorL2
+    momentoTRN      = argumentos.momentoTRN
 
+    capas        = np.asarray(capas)
+    tasaAprenTRN = np.float32(tasaAprenTRN)
+    momentoTRN   = np.float(momentoTRN)
+
+    # chequeos
+    assert dataset.find('.npz') != -1, "El conjunto de datos debe ser del tipo '.npz'"
+    assert porcentaje <= 1.0
 
     # configuraciones con respecto a los directorios
-    directorioActual= os.getcwd()                                  # directorio actual de ejecucion
-    rutaTest        = directorioActual + '/cupydle/test/face/'     # sobre el de ejecucion la ruta a los tests
-    rutaDatos       = directorioActual + '/cupydle/data/DB_face/'  # donde se almacenan la base de datos
-    carpetaTest     = directorio_ejecucion + '/'                   # carpeta a crear para los tests
-    rutaCompleta    = rutaTest + carpetaTest
+    directorioActual = os.getcwd()                                  # directorio actual de ejecucion
+    rutaTest         = directorioActual + '/cupydle/test/face/'     # sobre el de ejecucion la ruta a los tests
+    rutaDatos        = directorioActual + '/cupydle/data/DB_face/'  # donde se almacenan la base de datos
+    carpetaTest      = directorio + '/'                   # carpeta a crear para los tests
+    rutaCompleta     = rutaTest + carpetaTest
+    os.makedirs(rutaCompleta) if not os.path.exists(rutaCompleta) else None # crea la carpeta en tal caso que no exista
 
-    if not os.path.exists(rutaCompleta):        # si no existe la crea
-        print('Creando la carpeta para el test en: ',rutaCompleta)
-        os.makedirs(rutaCompleta)
-
-    if not os.path.exists(rutaDatos):
-        print("Creando la base de datos en:", rutaDatos)
-        os.makedirs(rutaDatos)
 
     # se cargan  los datos, debe ser un archivo comprimido, en el cual los
     # arreglos estan en dos archivos, 'videos' y 'clases'
-    b = numpy.load(rutaDatos + dataset)
+    b = np.load(rutaDatos + dataset)
     videos = b['videos']
     clases = b['clases']
     # las clases estan desde 1..6, deben ser desde 0..5
     clases -= 1
     del b #libera memoria
+
 
     # lista de tupas, [(x_trn, y_trn), ...]
     # los datos estan normalizados...
@@ -92,21 +107,20 @@ if __name__ == "__main__":
                        ruta=rutaCompleta)
 
     # se agregan los parametros
-    clasificador.setParametroEntrenamiento({'tasaAprendizaje':0.01})
-    clasificador.setParametroEntrenamiento({'regularizadorL1':0.00})
-    clasificador.setParametroEntrenamiento({'regularizadorL2':0.0001})
-    clasificador.setParametroEntrenamiento({'momento':0.0})
+    clasificador.setParametroEntrenamiento({'tasaAprendizaje':tasaAprenTRN})
+    clasificador.setParametroEntrenamiento({'regularizadorL1':regularizadorL1})
+    clasificador.setParametroEntrenamiento({'regularizadorL2':regularizadorL2})
+    clasificador.setParametroEntrenamiento({'momento':momentoTRN})
     clasificador.setParametroEntrenamiento({'activationfuntion':sigmoideaTheano()})
-    clasificador.setParametroEntrenamiento({'epocas':epocas})
+    clasificador.setParametroEntrenamiento({'epocas':epocasTRN})
     clasificador.setParametroEntrenamiento({'toleranciaError':0.08})
 
     # agrego tantas capas desee de regresion
     # la primera es la que indica la entrada
     # las intermedias son de regresion
     # la ultima es de clasificaicon
-    #clasificador.agregarCapa(unidadesEntrada=capas[0], unidadesSalida=capas[1], clasificacion=False, activacion=sigmoideaTheano(), pesos=None, biases=None)
-    #for idx, capa in enumerate(capas[0:-1]):
     for idx, _ in enumerate(capas[:-2]): # es -2 porque no debo tener en cuenta la primera ni la ultima
+        print(idx, capas[idx], capas[idx+1])
         clasificador.agregarCapa(unidadesEntrada=capas[idx], unidadesSalida=capas[idx+1], clasificacion=False, activacion=sigmoideaTheano(), pesos=None, biases=None)
     clasificador.agregarCapa(unidadesSalida=capas[-1], clasificacion=True, pesos=None, biases=None)
 
