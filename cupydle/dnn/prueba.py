fcfd8c2c6a80a0c70958752d6bf4959e03517cb3
Nelson Ponzoni <npcuadra@gmail.com>
2016-09-22 02:59:32 -0300

    pruebas

--- cupydle/dnn/prueba.py
+++ cupydle/dnn/prueba.py
@@ -4,11 +4,25 @@
 
 import itertools
 
-dictt={'a':[1,2,3],'b':['g', 1], 'c':[6]}
+parametros = {
+                'lr_pesos':  [0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.15],
+                'unidad':    ['binaria', 'gaussiana'],
+                'tamBatch':  [10],
+                'epocasTRN': [100,200,300],
+                'epocasFIT': [2000],
 
-varNames = sorted(dictt)
-combinations = [dict(zip(varNames, prod)) for prod in itertools.product(*(dictt[varName] for varName in varNames))]
+            }
 
+varNames = sorted(parametros)
+combinations = [dict(zip(varNames, prod)) for prod in itertools.product(*(parametros[varName] for varName in varNames))]
+
+print(type(combinations))
+print(type(combinations[0]))
 for i in combinations:
     print(i)
+
+print("Cantidad de Combinaciones posibles: ", len(combinations))
+tiempo_x_ejecucion = 6
+cantidad_dias = len(combinations) * tiempo_x_ejecucion / 24.0
+print("Cantidad de dias continuos de ejecucion: ", cantidad_dias)
 #print(combinations)
